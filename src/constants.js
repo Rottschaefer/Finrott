d80ff6e882a77d6184f32f18329d76fb589ea706
Rottschaefer <rottschaefer54@gmail.com>
2024-01-25 11:28:51 -0300

    Request URL fixed

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-// export const BASE_URL =
-//   "https://finrott-backend-rails-0065747041b6.herokuapp.com";
+export const BASE_URL =
+  "https://finrott-backend-rails-0065747041b6.herokuapp.com";
 
-export const BASE_URL = "http://localhost:3000";
+// export const BASE_URL = "http://localhost:3000";
